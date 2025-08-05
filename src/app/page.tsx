114482734c679138f9b2322fb12162b3e695483a
Dan Moffat <danmoffat@gmail.com>
2025-08-05 02:14:21 +0100

    Tailwind to the rescue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import HomeImage from '@/../public/home.jpg';
 export default function Home() {
   return (
     <div>
-      <Image src={HomeImage} alt='Factory' fill objectFit='cover' />
+      <Image src={HomeImage} alt='Factory' fill objectFit='cover' className='-z-10' />
     </div>
   );
 }
